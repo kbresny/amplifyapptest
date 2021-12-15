a15715f7d6fd0bac58549df36d56ab8360093a31
Kyle <kbresny@gmail.com>
2021-12-15 12:52:34 -0600

    change for class

--- src/App.js
+++ src/App.js
@@ -1,34 +1,16 @@
-import { Amplify } from 'aws-amplify';
-
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import { withAuthenticator } from '@aws-amplify/ui-react'
-import '@aws-amplify/ui-react/styles.css';
-
-import awsExports from './aws-exports';
-Amplify.configure(awsExports);
-
-function App({ signOut, user }) {
+function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <h1>We now have AUTH</h1>
-          <button onClick={signOut}>Sign out</button>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello Class</h1>
       </header>
     </div>
   );
 }
 
-export default withAuthenticator(App);
+export default App;
